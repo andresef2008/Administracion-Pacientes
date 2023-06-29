072f0827c3fb9b9abf460243eb8658fc8ba2fc19
Andres Figueroa <andresfiguef@gmail.com>
2023-06-29 16:15:16 -0500

    Primeros pasos con TailwindCss en el proyecto

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Header from './components/header.vue'
+import Formulario from './components/formulario.vue'
+
+</script>
+
+<template>
+  <div class="container mx-auto mt-20">
+    <Header />
+
+    <div class = "mt-12 md:flex">
+      <Formulario />
+      
+
+    </div>
+
+  </div>
+
+  
+  
+</template>
