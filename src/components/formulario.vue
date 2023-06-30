8e10f3a76a7e437038894ac984876b804193e390
Andres Figueroa <andresfiguef@gmail.com>
2023-06-30 17:03:08 -0500

    Finalizando Formulario

--- src/components/formulario.vue
+++ src/components/formulario.vue
@@ -10,6 +10,111 @@
         Añade Pacientes y
         <span class="text-indigo-600 font-bold">Adminístralos</span>
        </p>
+
+       <form
+        class = "bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+           
+       >
+       <div class="mb-5">
+        <label 
+            for="mascota"
+            class="block text-gray-700 uppercase font-bold"
+            >
+            Nombre Mascota
+        </label>
+
+        <input 
+            id="mascota"
+            type="text"
+            placeholder="Nombre de la mascota"
+            class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+
+       </div>
+
+       <div class="mb-5">
+        <label 
+            for="propietario"
+            class="block text-gray-700 uppercase font-bold"
+            >
+            Nombre Propietario
+        </label>
+
+        <input 
+            id="propietario"
+            type="text"
+            placeholder="Nombre del propietario"
+            class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+
+       </div>
+
+
+       <div class="mb-5">
+        <label 
+            for="email"
+            class="block text-gray-700 uppercase font-bold"
+            >
+            Email
+        </label>
+
+        <input 
+            id="email"
+            type="text"
+            placeholder="Email del propietario"
+            class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+
+       </div>
+
+       <div class="mb-5">
+        <label 
+            for="alta"
+            class="block text-gray-700 uppercase font-bold"
+            >
+            Alta
+        </label>
+
+        <input 
+            id="alta"
+            type="date"
+            class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+
+       </div>
+
+       <div class="mb-5">
+        <label 
+            for="sintomas"
+            class="block text-gray-700 uppercase font-bold"
+            >
+            Sintomas
+        </label>
+
+        <textarea
+            id="sintomas"
+            placeholder="Describe los sintomas de este paciente"
+            class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md h-40"
+          />
+
+       </div>
+
+       <input
+        type="submit"
+        class="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo 
+        cursor-pointer transition-colors"     
+        value="Registrar Paciente"
+    
+        />
+
+  
+       
+
+
+
+
+
+       </form>
        
     </div>
 
